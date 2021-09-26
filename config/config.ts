c3730bf025f4d3d141415646d346f40049dd3900
baijunyao <baijunyao@baijunyao.com>
2021-09-26 23:37:46 +0800

    Change path from `/ant` to `/admin`

--- config/config.ts
+++ config/config.ts
@@ -396,7 +396,7 @@ export default {
       changeOrigin: true,
     },
   },
-  base: '/ant/',
-  publicPath: '/ant/',
+  base: '/admin/',
+  publicPath: '/admin/',
   history: 'hash',
 } as IConfig;
